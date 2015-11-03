fd8317e725eb15ddb16df83bffcec2b499ce762e
edonnachie <edonnachie@users.noreply.github.com>
2015-11-03 10:43:06 +0100

    R CMD BATCH now with --no-restore
    .RData should not be loaded as can easily lead to errors.

--- R/run_at.R
+++ R/run_at.R
@@ -11,7 +11,14 @@ sleep_until <- function(time){
   Sys.sleep(d)
 	invisible()
 }
-#' Run a script immediately or at a specified time
+#' Run a script immediately or at a specified time.
+#' 
+#' By default, the script is run using
+#' 'R CMD BATCH  --no-restore --no-save'. This is often a more useful command than Rscript
+#' because it generates a log with input and output (e.g. useful for documentation
+#' and troubleshooting). The arguments "--no-restore --no-save" disable the loading and saving of
+#' session data (".RData" file), which is helps ensure reproducibility and avoids errors (e.g. a command
+#' fails , leading to the use of an outdated version of an object contained in .RData).
 #'
 #' @param script Location of script, input to R CMD BATCH or source
 #' @param time Time in format HH:MM
@@ -23,7 +30,7 @@ run_at <- function(script, time, method = "batch", args = NULL){
 	if(tolower(time) != "now") sleep_until(time)
 	t0 <- Sys.time()
   if(tolower(method) == "batch"){
-    system(paste0("R CMD BATCH --no-save ", args, script))
+    system(paste0("R CMD BATCH  --no-restore --no-save ", args, script))
   }
   if(tolower(method) == "rscript"){
     system(paste0("Rscript ", args, script))
